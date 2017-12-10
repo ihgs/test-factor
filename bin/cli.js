476f68acea66d424f18ab2e888f6579949c3be92
ihgs <hgs.dev@gmail.com>
2017-12-10 22:53:26 +0900

    Use commander

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
-const FactorAnalysis = require('../src/factorAnalysis');
 
-obj = new FactorAnalysis();
+const cmd = require('commander');
+const FactorAnalysis = require('../src/factorAnalysis');
+const version = require('../package.json').version;
+
+cmd.version(version);
+
+cmd.command('parse [input]').action(bundle);
+
+cmd.command('help [command]').action(function(command) {
+  let subcmd = cmd.commands.find(c => c.name() === command) || cmd;
+  subcmd.help();
+});
+
+var args = process.argv;
+if (!args[2] || !cmd.commands.some(c => c.name() === args[2])) {
+  args.splice(2, 0, 'help');
+}
+
+cmd.parse(args);
+
+function bundle(main, command) {
+  console.log(command.name());
+}
