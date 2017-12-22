59db6eb1598fd2e5df99766588b1c829434428cf
ihgs <hgs.dev@gmail.com>
2017-12-22 21:53:51 +0900

    Parse factor

--- src/factorAnalysis.js
+++ src/factorAnalysis.js
@@ -1,9 +1,95 @@
 const fs = require('fs');
 
+class Item {
+  constructor(no, type, label) {
+    this.no = no;
+    if (type == '.') {
+      this.normal = true;
+    }
+    this.label = label;
+  }
+
+  out() {
+    if (this.normal) {
+      console.log(`| ${this.no} ${this.label}`);
+    } else {
+      console.log(`| E ${this.no} ${this.label}`);
+    }
+  }
+}
+
+class Factor {
+  constructor(key, description) {
+    this.key = key;
+    this.description = description;
+
+    this.items = [];
+  }
+
+  addItem(no, type, label) {
+    this.items.push(new Item(no, type, label));
+  }
+  out() {
+    console.log(this.key);
+    this.items.forEach(function(item) {
+      item.out();
+    });
+  }
+}
+
+class Factors {
+  constructor() {
+    this.factorlist = [];
+    this.maxItemSize = 0;
+  }
+
+  add(line) {
+    const matchSeparator = line.match(/^-+/);
+    if (matchSeparator) {
+      if (this.started) {
+        return this;
+      } else {
+        this.started = true;
+      }
+      return;
+    }
+    const matchFactor = line.match(/^(\w+):\s*(.*)$/);
+    if (matchFactor) {
+      this.factor = new Factor(matchFactor[1], matchFactor[2]);
+      this.factorlist.push(this.factor);
+    }
+    const matchItem = line.match(/^\s+(\d+)([.-])\s+(.*)$/);
+    if (matchItem) {
+      this.factor.addItem(matchItem[1], matchItem[2], matchItem[3]);
+    }
+  }
+
+  out() {
+    this.factorlist.forEach(function(factor) {
+      factor.out();
+    });
+  }
+}
+
 class FactorAnalysis {
   constructor(filepath, options = {}) {
     var contents = fs.readFileSync(filepath);
-    console.log(contents.toString());
+    var factor = undefined;
+    contents
+      .toString()
+      .split('\n')
+      .forEach(function(line) {
+        if (line == '[factor]') {
+          factor = new Factors();
+        } else {
+          if (factor) {
+            if (factor.add(line)) {
+              factor.out();
+              factor = undefined;
+            }
+          }
+        }
+      });
   }
 }
 module.exports = FactorAnalysis;
