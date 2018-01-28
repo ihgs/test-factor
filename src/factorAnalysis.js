a0e5b4180b42b0ed0cc4315c776c6f8bad6b2fda
ihgs <hgs.dev@gmail.com>
2018-01-28 13:29:04 +0900

    Refactoring factoranalysis

--- src/factorAnalysis.js
+++ src/factorAnalysis.js
@@ -6,33 +6,32 @@ const fs = require('fs');
 class FactorAnalysis {
   constructor(filepath, options = {}) {
     const contents = fs.readFileSync(filepath);
-    let factor = undefined;
-    let combinations = undefined;
-    let lineNo;
+    let idx = 0;
+    let factor;
+    let combinations;
     try {
-      contents
-        .toString()
-        .split('\n')
-        .forEach(function(line, index) {
-          lineNo = index + 1;
-          if (line == '[factor]') {
-            factor = new Factors();
-          } else if (line == '[combination]') {
-            combinations = new Combinations(factor);
-            factor = undefined;
-          } else {
-            if (factor) {
-              if (factor.add(line)) {
-              }
-            }
-            if (combinations) {
-              if (combinations.add(line)) {
-              }
+      const lines = contents.toString().split('\n');
+      while (idx < lines.length) {
+        let line = lines[idx];
+        if (line.match(/^\[factor\]\s*$/)) {
+          factor = new Factors();
+          while ((line = lines[++idx])) {
+            if (factor.add(line)) {
+              break;
             }
           }
-        });
+        } else if (line.match(/^\[combination\]\s*$/)) {
+          combinations = new Combinations(factor);
+          while ((line = lines[++idx])) {
+            if (combinations.add(line)) {
+              break;
+            }
+          }
+        }
+        idx++;
+      }
     } catch (e) {
-      console.error(`Parse Error:(${lineNo}) ${e.message}`);
+      console.error(`Parse Error:(${idx + 1}) ${e.message}`);
       return;
     }
 
